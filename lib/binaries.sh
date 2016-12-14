1787fa0c4bd70112ab9dfa2ebfc97c39b0508df0
Hunter Loftis <hunter@hunterloftis.com>
2016-12-14 12:30:05 -0500

    use semver.io service to resolve yarn engines

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -9,15 +9,15 @@ needs_resolution() {
 
 install_yarn() {
   local dir="$1"
-  local version=${2:-latest}
+  local version="$2"
 
-  if [ "$version" = 'latest' ]; then
-    local download_url="https://yarnpkg.com/latest.tar.gz"
-  else
-    local download_url="https://yarnpkg.com/downloads/$version/yarn-v$version.tar.gz"
+  if needs_resolution "$version"; then
+    echo "Resolving yarn version ${version:-(latest)} via semver.io..."
+    local version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/yarn/resolve)
   fi
 
   echo "Downloading and installing yarn ($version)..."
+  local download_url="https://yarnpkg.com/downloads/$version/yarn-v$version.tar.gz"
   local code=$(curl "$download_url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/yarn.tar.gz --write-out "%{http_code}")
   if [ "$code" != "200" ]; then
     echo "Unable to download yarn: $code" && false
